cdc08f64849447411233bd4ac1ee541be4165825
JAlcon00 <c.jesusalmanza@gmail.com>
2024-02-20 16:31:20 -0600

    inicie a hacer merge sort, declarando el arreglo de ejemplo y el arreglo temporal.

---
+++ Merge/mergesort.java
@@ -0,0 +1,15 @@
+package Merge;
+ import java.util.*;
+
+public class mergesort {
+    public static void main(String[] args) {
+        //Arreglo de pruebas
+        int[] arr = {453, 656, 123, 44, 90, 500, 45, 999, 34};
+        int n = arr.length;
+        //inicializamos un arreglo temporal para realizar las operaciones a parte
+        int[] temp = new int[n];
+
+        
+    }
+    
+}
