08bcc6dbfb2173e105d4621bf3c151ce245f6caf
GaelDuran <ldt76578@lasallebajio.edu.mx>
2024-02-23 12:03:22 -0600

    Proceso del codigo

--- Radix/radixsort.java
+++ Radix/radixsort.java
@@ -3,5 +3,15 @@ package Radix;
 public class radixsort {
     public static void main(String[] args) {
         int[] arr = {22, 178, 249, 6, 18, 314};
+
+        radixsort(arr);
+
+        System.out.println("Arreglo Ordenado");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+            if (i < arr.length - 1) {
+                System.out.print(", ");
+            }
+
     }
 }
