682884a3f9fe5427fd588470a9df9660fd7726d4
JAlcon00 <c.jesusalmanza@gmail.com>
2024-02-23 13:35:02 -0600

    Quedo el merge sort pero falta aplicar el arreglo con valores random

--- Merge/mergesort.java
+++ Merge/mergesort.java
@@ -1,5 +1,8 @@
 package Merge;
  import java.util.*;
+ /*
+  * Hacer un merge sort a travez de porcentaje de reciduos
+  */
 
 public class mergesort {
     public static void main(String[] args) {
@@ -10,11 +13,61 @@ public class mergesort {
         int[] temp = new int[n];
 
 
-        //queda pendiente un cliclo anidado de comparación entre el arreglo original y el arreglo segundario
-        //Ciclo de iteración linear inicialmente de a 1 elemento, duplicándolo en cada turno 
+        //Se empieza a dividir los numeros en grupos más pequeños
         for(int tam = 1; tam < n; tam *= 2 ){
             // Recorrer el arreglo en segmentos del tamaño actual
+            //Aqui aplicamos la iteración para ordenarlos de 0 a n según su valor
             for(int ini = 0; ini < n - 1;ini += 2 * tam){
+                // Calcular el final del segmento actual
+                // Encontrar el punto medio y el final del segmento derecho
+                int fin = Math.min(ini + 2 * tam - 1, n - 1);
+                int mid = Math.min(ini + tam - 1, n - 1);
+                
+
+                //Aqui podemos fusionar los segmentos temporales
+                //Esto es para saber donde termina cada grupo de numeros que estamos ordenando
+                int i = ini, j = mid + 1, k = ini;
+
+
+                //Aqui podemos fusionar los segmentos temporales
+                while (i <= mid && j <= fin) {
+                    /*
+                     * Aqui va a comparar los numeros y determinará
+                     * cual numero es más grande y luego los va a regresar
+                     * a la caja vacia ordenados
+                     */
+                    if (arr[i] <= arr[j]) {
+                        temp[k++] = arr[i++];
+                    } else {
+                        temp[k++] = arr[j++];
+                    }
+                    
+                }
+
+                /////////////////////////////////////////////////
+                //Aqui se van a ordenar los numeros basado en su tamaño
+
+                while(j <= fin){
+                    temp[k++] = arr[j++];
+                }
+                
+                while (i <= mid){
+                    temp[k++] = arr[i++];
+                
+                }
+
+                //Se verifica que ningun dato se haya omitido
+
+                for(i = ini ; i <= fin; i++){
+                    arr[i] = temp[i];
+                }
+            
+
+
+
+
+
+
 
             }
         }
