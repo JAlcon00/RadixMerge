9a582fc4b67431cb795ce9f4f7fd1ec9d350770c
JAlcon00 <c.jesusalmanza@gmail.com>
2024-02-26 11:47:36 -0600

    se añade la función de imprimir iteración por iteración

--- Merge/mergesort.java
+++ Merge/mergesort.java
@@ -1,43 +1,39 @@
 package Merge;
 //import java.util.*;
 
-
- /*
-  * Author A: Almanza Contreras José de Jesús
-  * Author B: Alonso Romero Pablo Emilio
-  * Author C: Duran Torres Leonardo Gael
-    Hacer un programa que tome un arreglo caracteres aleatorios con n datos e implemente
-    el algoritmo de ordenamiento Merge Sort en el cual los datos se separan por dato, se mandan
-    a un arreglo temporal, se reordenan por dato, se reordenan desde la unidades, decenas, centenas y millares 
+/*
+ * Author A: Almanza Contreras José de Jesús
+ * Author B: Alonso Romero Pablo Emilio
+ * Author C: Duran Torres Leonardo Gael
+   Hacer un programa que tome un arreglo caracteres aleatorios con n datos e implemente
+   el algoritmo de ordenamiento Merge Sort en el cual los datos se separan por dato, se mandan
+   a un arreglo temporal, se reordenan por dato, se reordenan desde la unidades, decenas, centenas y millares 
 */
 
 public class mergesort {
     public static void main(String[] args) {
-        //Arreglo de pruebas
-        int[] arr = {453, 656, 123, 44, 90, 500, 45, 999, 34};
+        // Arreglo de pruebas
+        int[] arr = { 453, 656, 123, 44, 90, 500, 45, 999, 34 };
         int n = arr.length;
-        //inicializamos un arreglo temporal para realizar las operaciones a parte
+        // inicializamos un arreglo temporal para realizar las operaciones a parte
         int[] temp = new int[n];
 
-
-        //Se empieza a dividir los numeros en grupos más pequeños
-        for(int tam = 1; tam < n; tam *= 2 ){
+        // Se empieza a dividir los numeros en grupos más pequeños
+        for (int tam = 1; tam < n; tam *= 2) {
             // Recorrer el arreglo en segmentos del tamaño actual
-            //Aqui aplicamos la iteración para ordenarlos de 0 a n según su valor
-            for(int ini = 0; ini < n - 1;ini += 2 * tam){
+            // Aqui aplicamos la iteración para ordenarlos de 0 a n según su valor
+            for (int ini = 0; ini < n - 1; ini += 2 * tam) {
                 // Calcular el final del segmento actual
                 // Encontrar el punto medio y el final del segmento derecho
                 // se toman los grupos de numeros hasta que esten ordenados
                 int fin = Math.min(ini + 2 * tam - 1, n - 1);
                 int mid = Math.min(ini + tam - 1, n - 1);
-                
 
-                //Aqui podemos fusionar los segmentos temporales
-                //Esto es para saber donde termina cada grupo de numeros que estamos ordenando
+                // Aqui podemos fusionar los segmentos temporales
+                // Esto es para saber donde termina cada grupo de numeros que estamos ordenando
                 int i = ini, j = mid + 1, k = ini;
 
-
-                //Aqui podemos fusionar los segmentos temporales
+                // Aqui podemos fusionar los segmentos temporales
                 while (i <= mid && j <= fin) {
                     /*
                      * Aqui va a comparar los numeros y determinará
@@ -49,43 +45,50 @@ public class mergesort {
                     } else {
                         temp[k++] = arr[j++];
                     }
-                    
+
                 }
 
                 /////////////////////////////////////////////////
-                //Aqui se van a ordenar los numeros basado en su tamaño
+                // Aqui se van a ordenar los numeros basado en su tamaño
 
-                while(j <= fin){
+                while (j <= fin) {
                     temp[k++] = arr[j++];
                 }
-                
-                while (i <= mid){
+
+                while (i <= mid) {
                     temp[k++] = arr[i++];
-                
+
                 }
 
-                //Se verifica que ningun dato se haya omitido
+                // Se verifica que ningun dato se haya omitido
 
-                for(i = ini ; i <= fin; i++){
+                for (i = ini; i <= fin; i++) {
                     arr[i] = temp[i];
                 }
-            
 
-
-
-
-
-
-
+                /*
+                 * Esta serie de impresiones es para importar ImprimirArreglo para que se
+                 * imprima
+                 * iteración por iteración.
+                 */
+                System.out.println("\nDespues de fusionar desde " + ini + " hasta " + fin + ": ");
+                imprimirArreglo(arr);
             }
+            System.out.println("\nDespues de completar segmentos de tamaño " + tam + ": ");
+            imprimirArreglo(arr);
         }
+        System.out.println("\nArreglo ordenado: ");
+        imprimirArreglo(arr);
+    }
 
-    
-        
-        for (int i = 0; i < n; i++) {
+    // Se crea ImprimirArreglo a travez de un ciclo iterativo para que se
+    // puedan apreciar los cambios del ordenamiento
+
+    public static void imprimirArreglo(int[] arr) {
+
+        for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
-
-        
+        System.out.println();
     }
-}    
+}
